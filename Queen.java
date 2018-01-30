a3386f2fea7bceddb7919b826cbc556f8a3bb596
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2018-01-30 13:13:17 -0500

    Update Queen.java

--- Queen.java
+++ Queen.java
@@ -2,4 +2,20 @@ package theChessBot;
 
 public class Queen extends Piece {
 
+	public Queen ( boolean isWhite) {
+		super(isWhite);
+		System.out.println("Creating a " +(isWhite?"white":"black")+ " queen");
+
+	}
+	
+	public String toString () {
+		return (isWhite?"white":"black") +" queen";
+	}
+	
+	public String getType () {
+		return "Queen";
+	}
+//	public Queen ( int position ) {
+//		this.position = position;
+//	}
 }
