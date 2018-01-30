310c5c5ee12530249f7967ab50327ef7c6c26e5f
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2018-01-30 13:06:10 -0500

    Update King.java

--- King.java
+++ King.java
@@ -2,4 +2,20 @@ package theChessBot;
 
 public class King extends Piece {
 
+	public King ( boolean isWhite) {
+		super( isWhite );
+		System.out.println("Creating a " +(isWhite?"white":"black")+ " king");
+
+	}
+	
+	public String toString () {
+		return (isWhite?"white":"black") +" king";
+	}
+	
+	public String getType () {
+		return "King";
+	}
+//	public King ( int position ) {
+//		this.position = position;
+//	}
 }
