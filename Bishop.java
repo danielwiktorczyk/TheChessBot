ff51051001e435da485db0ac85b2e6b4ff48c27b
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2018-01-30 13:05:33 -0500

    BishopWithtoString

--- Bishop.java
+++ Bishop.java
@@ -2,4 +2,25 @@ package theChessBot;
 
 public class Bishop extends Piece {
 
+	
+	
+	public Bishop ( boolean isWhite) {
+		super( isWhite);
+		System.out.println("Creating a " +(isWhite?"white":"black")+ " bishop");
+
+	}
+	
+	public String toString () {
+		return (isWhite?"white":"black") +" bishop";
+	}
+	
+	public String getType () {
+		return "Bishop";
+	}
+	
+//	
+//	public Bishop ( int position ) {
+//		this.position = position;
+//	}
+	
 }
