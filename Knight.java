9d8ac828a2e4432007997527d288468b2d5943eb
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2018-01-30 13:06:44 -0500

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -2,4 +2,21 @@ package theChessBot;
 
 public class Knight extends Piece{
 
+	public Knight ( boolean isWhite) {
+		super( isWhite );
+		System.out.println("Creating a " +(isWhite?"white":"black")+ " knight");
+	}
+	
+	public String toString () {
+		return (isWhite?"white":"black") +" knight";
+	}
+	
+	public String getType () {
+		return "Knight";
+	}
+//	public Knight ( int position ) {
+//		this.position = position;
+//	}
+	
+	
 }
