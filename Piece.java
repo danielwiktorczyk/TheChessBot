43fbdfa1232aa7893431bff66559a4e3d25a43b3
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2018-01-30 13:13:50 -0500

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -2,4 +2,16 @@ package theChessBot;
 
 public class Piece {
 
+	protected int position; // goes from 0 to 63
+	protected boolean isWhite;
+	
+	public Piece ( boolean isWhite) {
+		this.isWhite = isWhite;
+//		System.out.println("Creating a " +(isWhite?"white":"black")+ " piece");
+	}
+	
+	public String getType () {
+		return "Piece";
+	}
+		
 }
