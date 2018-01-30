a18e6a3d3ea63aa5cf29cfb6271e6c8068800b5e
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2018-01-30 13:07:37 -0500

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -2,4 +2,22 @@ package theChessBot;
 
 public class Pawn extends Piece {
 
+	public Pawn ( boolean isWhite) {
+		super( isWhite);
+		System.out.println("Creating a " +(isWhite?"white":"black")+ " pawn");
+		
+	}
+	
+	public String toString () {
+		return (isWhite?"white":"black") +" pawn";
+	}
+	
+	public String getType () {
+		return "Pawn";
+	}
+	
+//	public Pawn ( int position ) {
+//		this.position = position;
+//	}
+	
 }
