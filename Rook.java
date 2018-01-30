f82182f5bf41e0559ce8f01e9836bfd69b8596bc
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2018-01-30 13:12:27 -0500

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -2,4 +2,22 @@ package theChessBot;
 
 public class Rook extends Piece{
 
+	public Rook ( boolean isWhite) {
+		super( isWhite);
+		System.out.println("Creating a " +(isWhite?"white":"black")+ " rook");
+	}
+	
+	public String toString () {
+		return (isWhite?"white":"black") +" rook";
+	}
+	
+	public String getType () {
+		return "Rook";
+	}
+//	
+//	public Rook ( int position ) {
+//		this.position = position;
+//	}
+	
+	
 }
