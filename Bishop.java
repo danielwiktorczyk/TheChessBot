2a879a3123a6fc6dbf9a5547043bc43609336962
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2018-02-02 18:35:39 -0500

    Fixed something rotten in Bishop

--- Bishop.java
+++ Bishop.java
@@ -1,5 +1,17 @@
 package theChessBot;
 
-public class Bishop extends Piece {
+public class Bishop extends Piece{
 
+	public Bishop ( boolean isWhite) {
+		super( isWhite );
+		//System.out.println("Creating a " +(isWhite?"white":"black")+ " knight");
+	}
+	
+	public String toString () {
+		return (isWhite?"white":"black") +" bishop";
+	}
+	
+	public String getType () {
+		return "bishop";
+	}
 }
