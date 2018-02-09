9eb492127f879f0d30e6a35bb2abc77aac04cf00
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2018-02-09 13:23:32 -0500

    Update King.java

--- King.java
+++ King.java
@@ -4,11 +4,11 @@ public class King extends Piece {
 
   public King ( boolean isWhite) {
 		super( isWhite );
-		//System.out.println("Creating a " +(isWhite?"white":"black")+ " knight");
+		//System.out.println("Creating a " +(isWhite?"white":"black")+ " king");
 	}
 	
 	public String toString () {
-		return (isWhite?"white":"black") +" knight";
+		return (isWhite?"white":"black") +" king";
 	}
 	
 	public String getType () {
