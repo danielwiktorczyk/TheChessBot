36ca1e80dc55785b78443c29510aa4f7afa09b68
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2018-02-10 13:15:59 -0500

    Cleaner, and has a greeting

--- BoardDriver.java
+++ BoardDriver.java
@@ -8,71 +8,34 @@ public class BoardDriver {
 		
 		Board mainBoard = new Board();
 		Scanner keys = new Scanner(System.in);
-		
-		
-//		System.out.println((int) 'A');
-//		System.out.println((int) 'H');
-//		System.out.println((int) '1');
-//		
-//		
-//		System.out.println(((int) "A1".charAt(1))-48);
-//		
-//		System.out.println(mainBoard.pieceAt("A1"));
-//		System.out.println(mainBoard.pieceAt("A2"));
-//		System.out.println(mainBoard.pieceAt("C4"));
-//		System.out.println(mainBoard.pieceAt("H8"));
-//		
-//		mainBoard.display();
-//		
-////		mainBoard.move("#2","A3");
-////		mainBoard.move("I2","A3");
-////		mainBoard.move("A2","A9");
-//		
-//		mainBoard.move("A2","A3");
-//		mainBoard.display();
-//		
-//		mainBoard.move("A7","A6");
-//		mainBoard.display();
-//		
-////		mainBoard.move("A6","A4");
-//
-//		
-//		mainBoard.display();
-//		
-//		mainBoard.move("A3","A4");
-//		mainBoard.display();
-//		
-//		mainBoard.move("A1","A3");
-//		mainBoard.display();
-//		
-//		mainBoard.move("A3","A7");
-		
-		mainBoard.display();
-		
+		int moveNumberOfLastTurn = 0;
 		boolean go = true;
 		String initialLocation;
 		String finalLocation;
 		
+		
+
 		System.out.println();
-		System.out.println("Welcome!");
-		System.out.println();
+		System.out.print("\\\\~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~//\n" 
+				+ "      Welcome to Steve, the Chess Program! \n"
+				+ "//~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~\\\\\n");
+		mainBoard.display();
 		
 		while (go) {
 			
-			System.out.println("Enter initial location of piece to move:");
+			moveNumberOfLastTurn = mainBoard.getMoveNumber();
+			System.out.println("\nEnter initial location of piece to move:");
 			initialLocation = keys.next();
 			System.out.println("Enter final location of piece to move:");
 			finalLocation = keys.next();
 			
+			
 			mainBoard.move( initialLocation , finalLocation );
-			mainBoard.display();
 			
-//			System.out.println("Continue? y / n");
-//			if (keys.next().equalsIgnoreCase("n"))
-//				go = false;
-			
-			
-			
+			if ( mainBoard.getMoveNumber() != moveNumberOfLastTurn ) {
+				mainBoard.display();
+			}
+		
 		}
 
 		keys.close();
